196559cb4373ef96e6a33e8b7fb8c9c6563b2878
abdelhadia72 <thisisavnb@gmail.com>
2024-04-08 16:40:28 +0000

    arrow fun

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,16 @@
+/**
+ * Represents a task block.
+ * @param {boolean} trueOrFalse - Indicates whether the task is true or false.
+ * @returns {Array} An array containing the values of task and task2.
+ */
+
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
--- 0x00-ES6_basic/sum.js
+++
@@ -1,6 +0,0 @@
-function sum(a, b) {
-  console.log("Xmark")
-  return a + b;
-}
-sum(1,2); 
-module.exports = sum;
