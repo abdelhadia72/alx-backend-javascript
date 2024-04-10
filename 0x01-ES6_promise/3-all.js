b285cbdc4491124260a7c62ab68d81b214dd77e9
abdelhadia72 <thisisavnb@gmail.com>
2024-04-10 23:51:22 +0000

    fixing all in task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,17 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
-    })
-    .catch(() => {
-      console.log("Signup system offline");
-    });
+async function handleProfileSignup() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser(),
+    ]);
+    console.log(
+      `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`,
+    );
+  } catch (error) {
+    console.error('Signup system offline');
+  }
 }
+
+export default handleProfileSignup;
