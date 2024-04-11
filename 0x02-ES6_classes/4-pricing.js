47f6a724dc6100bb4233dc88bfbed3fa179e82c7
abdelhadia72 <thisisavnb@gmail.com>
2024-04-11 13:16:29 +0000

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount === 'number' && currency instanceof Currency) {
+      this._amount = amount;
+      this._currency = currency;
+    } else {
+      throw new Error('Invalid data!');
+    }
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof amount === 'number') {
+      this._amount = value;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  convertPrice(conversionRate) {
+    if (typeof conversionRate === 'number') {
+      return this._amount * conversionRate;
+    }
+    throw new TypeError('conversionRate must be a number');
+  }
+}
