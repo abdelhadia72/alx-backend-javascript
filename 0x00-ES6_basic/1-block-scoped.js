d2b792d4938858961a77c13e3bdf49bf84f9428d
abdelhadia72 <thisisavnb@gmail.com>
2024-04-11 00:34:10 +0000

    eslint fixing task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,3 @@
-/**
- * Represents a task block.
- * @param {boolean} trueOrFalse - Indicates whether the task is true or false.
- * @returns {Array} An array containing the values of task and task2.
- */
-
 export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
