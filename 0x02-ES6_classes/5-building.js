704cc3f36941cc6e999af04b31587d03293a3565
abdelhadia72 <thisisavnb@gmail.com>
2024-04-11 15:23:49 +0000

    building (05)

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,7 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft === "number") {
-      this._sqft = sqft;
-    } else {
-      throw new TypeError("sqft must be a number");
-    }
+    this._sqft = sqft;
+    if (!(new.target === Building)) this.evacuationWarningMessage();
   }
 
   get sqft() {
@@ -12,8 +9,9 @@ export default class Building {
   }
 
   evacuationWarningMessage() {
-    throw new Error(
-      "Class extending Building must override evacuationWarningMessage"
-    );
+    if (!(this === Building))
+      throw new Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
   }
 }
