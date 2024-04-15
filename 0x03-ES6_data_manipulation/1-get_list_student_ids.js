c2d6617f2cf0fff8f8fe987e4e56d4b2d8da4c5b
abdelhadia72 <thisisavnb@gmail.com>
2024-04-15 16:06:00 +0100

    Task from 0 to 2

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+export default function getListStudentIds(args) {
+  if (!Array.isArray(args)) {
+    return [];
+  }
+
+  return args.map((arg) => arg.id);
+}
--- 0x03-ES6_data_manipulation/README.md
+++ 0x03-ES6_data_manipulation/README.md
@@ -1,2 +1,1 @@
 # 0x03-ES6_data_manipulation
-
