e9d38bd07187a38161312c9805f4c952fcbe14f8
abdelhadia72 <thisisavnb@gmail.com>
2024-04-15 19:00:16 +0100

    Task 9 done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,18 @@
-export default function updateStudentGradeByCity(students, city, grade) {
-  return;
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const newGrade = newGrades.reduce((grade, newGrade) => {
+        if (newGrade.studentId === student.id) {
+          return newGrade.grade;
+        }
+        return grade;
+      }, "N/A");
+      return {
+        ...student,
+        grade: newGrade,
+      };
+    });
 }
+
+export default updateStudentGradeByCity;
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  const result = [];
+  if (!startString || typeof startString !== "string") {
+    return "";
+  }
+
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+
+  return result.join("-");
+}
