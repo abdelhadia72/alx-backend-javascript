3fc29a6e614ee0e699109b7f7a791ea4740e9673
abdelhadia72 <thisisavnb@gmail.com>
2024-04-16 17:00:59 +0100

    task 8 fixing

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,16 @@
 export default function cleanSet(set, startString) {
   const result = [];
-  if (!startString || typeof startString !== "string") {
-    return "";
+  if (!startString || typeof startString !== 'string') {
+    return '';
   }
 
   for (const value of set) {
-    if (value.startsWith(startString)) {
-      if (typeof value === "string") {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      if (value.length > startString.length) {
         result.push(value.slice(startString.length));
       }
     }
   }
 
-  return result.join("-");
+  return result.join('-');
 }
