89448534def0f28b5e6e7b887d7b6ab62fe5e47e
abdelhadia72 <thisisavnb@gmail.com>
2024-04-16 18:17:39 +0100

    Task 100 api limit

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,15 @@
-export function weakMap() {
-  return;
+const weakMap = new WeakMap();
+
+const limit = 5;
+
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= limit) {
+    throw new Error('Endpoint load is high');
+  }
 }
 
-export function queryAPI() {
-  return;
-}
+export { queryAPI, weakMap };
